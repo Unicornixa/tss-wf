9d69d8e28b53935410a2ff6538ffa1f0ca9150a9
Unicornixa <138890462+Unicornixa@users.noreply.github.com>
2026-03-10 22:29:28 +0200

    Update BASE URL to include dynamic REF

--- js/form/email-form.js
+++ js/form/email-form.js
@@ -1,5 +1,6 @@
 (function () {
-  const BASE = "https://cdn.jsdelivr.net/gh/unicornixa/tss-wf@main/js";
+  const REF = "58b3eeb"
+  const BASE = "https://cdn.jsdelivr.net/gh/unicornixa/tss-wf@${REF}/js";
 
   function loadScript(src) {
     return new Promise((resolve, reject) => {
